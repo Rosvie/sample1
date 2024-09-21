7d874bc6fcda8d8e2acc0c66139a1ebc0c78a5be
Rosvie Pepito <pepitorosvie@gmail.com>
2024-09-21 22:50:20 +0800

    UP slider and burger menu

--- js/javascript.js
+++ js/javascript.js
@@ -23,20 +23,53 @@
     
 
     // burger menu
-    const burger = document.querySelector("#hamburger_menu");
-    const navMenu = document.querySelector(".nav_menu");
+    // const burger = document.querySelector("#hamburger_menu");
+    // const navMenu = document.querySelector(".nav_menu");
 
-    burger.addEventListener("click", () => {
-    burger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-    });
+    // burger.addEventListener("click", () => {
+    // burger.classList.toggle("active");
+    // navMenu.classList.toggle("active");
+    // });
 
-    document.querySelectorAll(".nav_menu li a").forEach((n) =>
-    n.addEventListener("click", () => {
-        burger.classList.remove("active");
-        navMenu.classList.remove("active");
-    })
-    );
+    // document.querySelectorAll(".nav_menu li a").forEach((n) =>
+    // n.addEventListener("click", () => {
+    //     burger.classList.remove("active");
+    //     navMenu.classList.remove("active");
+    // })
+    // );
 
+
+  //   document.getElementById('hamburger_menu').addEventListener('click', function() {
+  //     // Toggle 'active' class on both hamburger and nav menu
+  //     this.classList.toggle('active');
+  //     document.querySelector('.nav_menu').classList.toggle('active');
+  // });
+
+
+  document.getElementById('hamburger_menu').addEventListener('click', function() {
+    // Toggle 'active' class on both hamburger and nav menu
+    this.classList.toggle('active');
+    document.querySelector('.nav_holder').classList.toggle('active');
+});
+
+
+// Slider
+const slidesContainer = document.getElementById("slider_boxes");
+const slide = document.querySelector(".slider");
+const prevButton = document.getElementById("prev");
+const nextButton = document.getElementById("next");
+
+nextButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+});
+
+
+  
 
 // });
